e0ad02f7ffc605e15c4241df7cd5cd225949fb56
Matt Price <105237637+Matt-239@users.noreply.github.com>
2023-04-13 21:12:53 -0400

    adds admin controller, admin user view

---
+++ assignment1/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+using assignment1.Data;
+using assignment1.Libs;
+using assignment1.Models;
+using assignment1.Models.Users;
+using assignment1.Models.Generics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assignment1.Controllers
+{
+    [Route("Admin")]
+    public class AdminController : BaseController
+    {
+        public AdminController(ILogger<AdminController> _logger) : base(_logger) { }
+
+        [HttpGet("AdminPage")]
+        public IActionResult AdminPage()
+        {
+            if (!Request.Cookies.ContainsKey(Persistent.UserSession_Cookie)) return RedirectToAction("Index", "Home");
+
+            UserBase user = this.RecoverUserSession();
+
+            if (user == null) return Forbid();
+            else if (user.UserTypeId != Persistent.user_type_table.FirstOrDefault(x => x.UserTypeName.Equals("Admin")).UserTypeId) return Forbid();
+
+            return View(new LayoutModel<IEnumerable<UserBase>>()
+            {
+                User = user,
+                Menus = this.GetMenus(user),
+                Data = new DBConnector().GetAllUsers(),
+            });
+        }
+
+        [HttpGet("AdminEditUser")]
+        public IActionResult AdminEditUser([FromQuery(Name = "uid")] int? _uid)
+        {
+            if (!Request.Cookies.ContainsKey(Persistent.UserSession_Cookie)) return RedirectToAction("Index", "Home");
+
+            UserBase user = this.RecoverUserSession();
+
+            if (user == null) return Forbid();
+
+            else if (user.UserTypeId != Persistent.user_type_table.FirstOrDefault(x => x.UserTypeName.Equals("Admin")).UserTypeId) return Forbid();
+
+            if (!_uid.HasValue) return RedirectToAction("AdminPage", "Admin");
+
+            UserBase other = new DBConnector().GetUser(_uid.Value);
+
+            if (other == null) return RedirectToAction("AdminPage", "Admin");
+
+            return RedirectToAction("EditProfile", "User", new { uid = _uid });
+        }
+    }
+}
