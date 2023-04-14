dc1f6cd3fff1eba82138d2193ac82b1143e76a07
Matt Price <105237637+Matt-239@users.noreply.github.com>
2023-04-14 18:04:06 -0400

    adds admin capabilities

--- assignment1/Controllers/AdminController.cs
+++ assignment1/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using assignment1.Models;
 using assignment1.Models.Users;
 using assignment1.Models.Generics;
 using Microsoft.AspNetCore.Mvc;
+using assignment1.Models.Auction;
 
 namespace assignment1.Controllers
 {
@@ -56,8 +57,9 @@ namespace assignment1.Controllers
         }
 
         [HttpPost("AdminEditUser")]
-        public IActionResult AdminEditUser(ProfileUser _user)
-        {
+        public IActionResult AdminEditUser(int id, ProfileUser _user)
+        {   
+            
             if (!Request.Cookies.ContainsKey(Persistent.UserSession_Cookie)) return RedirectToAction("Index", "Home");
 
             UserBase user = this.RecoverUserSession();
@@ -68,19 +70,69 @@ namespace assignment1.Controllers
 
             if (_user == null) return RedirectToAction("AdminPage", "Admin");
 
-            UserBase other = new DBConnector().GetUser(_user.Id);
+            UserBase other = new DBConnector().GetUser(id);
 
-            if (other == null) return RedirectToAction("Index", "Home");
+            if (other == null) return RedirectToAction("AdminPage", "Admin");
 
+       
+            other.Username = _user.Username;
+            other.Email = _user.Email;
             other.FirstName = _user.FirstName;
             other.LastName = _user.LastName;
-            other.Email = _user.Email;
-            other.UserTypeId = _user.UserTypeId;
 
-            new DBConnector().UpdateUser(other);
+            new DBConnector().UpdateUser(other, true);
 
             return RedirectToAction("AdminPage", "Admin");
         }
 
+        [HttpGet("AdminAuction")]
+        public IActionResult AdminAuction([FromQuery(Name = "aid")] int? _aid)
+        {
+            if (!Request.Cookies.ContainsKey(Persistent.UserSession_Cookie)) return RedirectToAction("Index", "Home");
+
+            UserBase user = this.RecoverUserSession();
+
+            if (user == null) return Forbid();
+
+            else if (user.UserTypeId != Persistent.user_type_table.FirstOrDefault(x => x.UserTypeName.Equals("Admin")).UserTypeId) return Forbid();
+
+            if (!_aid.HasValue) return RedirectToAction("AdminPage", "Admin");
+
+            IEnumerable<AuctionModel> auction = new DBConnector().GetLastAuctions(DBConnector.LandingPageAuctionsOptions.Last50);
+
+            if (auction == null) return RedirectToAction("AdminPage", "Admin");
+
+            return View(new LayoutModel<IEnumerable<AuctionModel>>()
+            {
+                User = user,
+                Menus = this.GetMenus(user),
+                Data = auction
+            });
+        }
+
+        [HttpPost("AdminEditAuction")]
+        public IActionResult AdminEditAuction(int id, AuctionModel _auction)
+        {
+            if (!Request.Cookies.ContainsKey(Persistent.UserSession_Cookie)) return RedirectToAction("Index", "Home");
+
+            UserBase user = this.RecoverUserSession();
+
+            if (user == null) return Forbid();
+
+            else if (user.UserTypeId != Persistent.user_type_table.FirstOrDefault(x => x.UserTypeName.Equals("Admin")).UserTypeId) return Forbid();
+
+            if (_auction == null) return RedirectToAction("AdminAuctions", "Admin");
+
+            AuctionModel auction = new DBConnector().GetAuction(id);
+
+            if (auction == null) return RedirectToAction("AdminAuctions", "Admin");
+
+            auction.AuctionName = _auction.AuctionName;
+            
+
+            new DBConnector().UpdateAuction(auction, true);
+
+            return RedirectToAction("AdminPage", "Admin");
+        }
     }
 }
